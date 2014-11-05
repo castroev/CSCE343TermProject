5264bdac559afc5974be6106efaa870151ef9f70
Ermenildo Castro <castroev@plu.edu>
2014-11-04 20:09:32 -0800

    Revision
    11/04/14

--- LEGACY/read.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-console.log("Starting");
-fs.readFile("./files/sample.txt", function(error, data) {
-	console.log("Contents: " + data);
-});
-console.log("Carry on executing");
--- LEGACY/read_sync.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-console.log("Starting to read the file");
-var content = fs.readFileSync("./files/sample.txt", "utf8");
-console.log("File contents: " + content);
-console.log("Carry on executing");
